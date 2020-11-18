5303f08214c494ed89b524292ac7146cbd0d2105
elijahsamuels <elijahsamuels@gmail.com>
2020-11-17 20:06:36 -0700

    commit tree 9a9a9ca455aff7d0dea0da31573783e032b22922

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,51 @@
+# book.rb
+
+class Book
+    
+
+# attr_accessor :title #yes, we could do this instead of getter/setter methods. The guide isn't asking for it yet.
+
+
+attr_accessor :author, :page_count, :genre
+attr_reader :title
+
+    def initialize(title)
+        @title = title
+    end
+
+    # def title
+    #     @title
+    # end
+
+
+    # def author=(author)
+    #     @author = author
+    # end
+
+    # def author
+    #     @author
+    # end
+
+    # def page_count=(page_count)
+    #     @page_count = page_count
+    # end
+
+    # def page_count
+    #     @page_count
+    # end
+
+    # def genre=(genre)
+    #     @genre = genre
+    # end
+
+    # def genre
+    #     @genre
+    # end
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
 
 
